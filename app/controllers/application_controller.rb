d08a596f6c9cf0c09b931a928035f0ea2e9e2282
Tan Trung Vo <tantrungse@gmail.com>
2025-06-20 14:21:10 +0700

    implement login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_in, keys: %i[role])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[role])
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     # devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
   end
